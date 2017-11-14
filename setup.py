3c2d356a13d644c4c6e5be4c01ff4a1da9665e8d
himanshu3006 <32892920+himanshu3006@users.noreply.github.com>
2017-11-14 12:36:32 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import codecs
+from setuptools import setup, find_packages
+#from distribute import setup, find_packages
+import djintegration
+
+import os
+templates_dirs = []
+for directory in os.walk('djintegration/templates'):
+    templates_dirs.append(directory[0][14:]+'/*.*')
+
+for directory in os.walk('djintegration/tests'):
+    templates_dirs.append(directory[0][14:]+'/*.txt')
+
+print templates_dirs
+
+setup(
+    name='djintegration',
+    version=djintegration.__version__,
+    description=djintegration.__doc__,
+    author=djintegration.__author__,
+    author_email=djintegration.__contact__,
+    url=djintegration.__homepage__,
+    platforms=["any"],
+    packages=find_packages(exclude=['testproj', 'testproj.*']),
+    package_data={'djintegration': templates_dirs},
+    zip_safe=False,
+    test_suite="nose.collector",
+    install_requires=(
+        'nose',
+        'django',
+        'virtualenv',
+        #'coverage',
+    ),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Framework :: Django",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    long_description=codecs.open('README', "r", "utf-8").read(),
+)
